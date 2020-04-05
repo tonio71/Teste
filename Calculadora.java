faa678c51f78970e66d4915f80276a61d1a7f057
tonio71 <tonio.maria@gmail.com>
2020-04-05 11:01:52 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+package junit.aplicativos;
+  /**
+   *
+   * @autor Diogo
+  */
+ public class Calculadora{
+  public double somar (double num1, double num2){
+    return 0.0;
+  }
+  public double subtrair (double num1, double num2){
+    return 0.0;
+  }
+  public double multiplicar (double num1, double num2){
+    return 0.0;
+  }
+  public double dividir (double num1, double num2){
+    return 0.0;
+  }
+}
